4a199b7e78fc39524e9f4bf0cb74f515b4f85b44
10pc <108258202+10pc@users.noreply.github.com>
2023-06-09 05:40:27 +0000

    no

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+
+
+# UPDATING DISCORD.PY FUCKED EVERYTHING UP HAHAHAHAHAHA
+
+
 import discord
 import os
 import requests
-#again
-client = discord.Client()
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+class mapList():
+  def __init__(self, db = []):
+    self.db = db
+    
+  def newMap(self, map):
+    self.db.append(map)
+
+class map():
+  def __init__(self, name, mapId, parentSetId, sr, bpm, totalLength, hitLength, ar, cs, od, combo):
+    self.name = name
+    self.mapId = mapId
+    self.parentSetId = parentSetId
+    self.sr = sr
+    self.bpm = bpm
+    self.totalLength = totalLength
+    self.hitLength = hitLength
+    self.ar = ar
+    self.cs = cs
+    self.od = od
+    self.combo = combo
+
+ls = mapList()
+
+def sec(seconds):
+  m = seconds // 60
+  s = seconds % 60
+  return f'{m}:{s}'
 
 @client.event
 async def on_ready():
@@ -12,19 +46,43 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+@client.event
+async def on_message(message):
   if message.content.startswith('https://osu.ppy.sh/'):
-    if (message.channel.id == 1115863989612728381):
-      url = message.content.split("/")
-      id = url[-1]
-      r = requests.get(f"https://api.chimu.moe/v1/map/{id}")
-      r = r.json()
-      mapName = r["OsuFile"]
+    print("hsdjgfalkjsd")
+    url = message.content.split("/")
+    id = url[-1]
+    r = requests.get(f"https://api.chimu.moe/v1/map/{id}")
+    r = r.json()
       
-      embed=discord.Embed(description=f'**Star:** {r["DifficultyRating"]} **BPM:** {r["BPM"]} **Length:** {r["TotalLength"]} ({r["HitLength"]})\n**[Preview](https://osu-preview.jmir.ml/preview#{id})**')
-      embed.set_author(name=mapName[:len(mapName) - 4], url=message.content)
-      embed.add_field(name="", value=f'**AR:** {r["AR"]} ● **CS:** {r["CS"]} ● **OD:** {r["OD"]}\n**Max Combo:** {r["MaxCombo"]}x', inline=True)
-      embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{r["ParentSetId"]}/covers/cover.jpg')
-      await message.channel.send(embed=embed)
+    mapName = r["OsuFile"]
+    totalL = sec(r["TotalLength"])
+    hitL = sec(r["HitLength"])
+
+    ls.newMap(map(mapName, id, r["ParentSetId"], r["DifficultyRating"], r["BPM"], totalL, hitL, r["AR"], r["CS"], r["OD"], r["MaxCombo"]))
+
+    success = discord.Embed(description = "BN's have been notified, please wait.", color = 0x00ff40)
+    success.set_author(name = "Request sent!")
+    await message.channel.send(embed=success)
+    
+    embed = discord.Embed(title = mapName[:len(mapName) - 4], url=message.content, description=f'**Star:** {r["DifficultyRating"]} **BPM:** {r["BPM"]} **Length:** {r["TotalLength"]} ({r["HitLength"]})\n**[Preview](https://osu-preview.jmir.ml/preview#{id})**', color = 0x0c0aa4)
+    embed.set_author(name = "New request!")
+    embed.add_field(name="", value=f'**AR:** {r["AR"]} **CS:** {r["CS"]} **OD:** {r["OD"]}\n**Max Combo:** {r["MaxCombo"]}x', inline=True)
+    embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{r["ParentSetId"]}/covers/cover.jpg')
+
+    view = discord.ui.View()
+    button = discord.ui.Button(Label = "click")
+    view.add_item(button)
+      
+    c = client.get_channel(1059771218271678524)
+    await c.send(embed = embed, view = view)
+
+  if message.content.startswith('list'):
+    ls.listMaps()
 
 try:
   client.run(os.environ["TOKEN"])
